a63cb97188cbc2047026262a269f7cb622f0b005
urevoleg <urevolegg@gmail.com>
2021-11-06 14:37:50 +0300

    fix

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 from app import app, api
 from app.models import Time
+from flask import request
+
+import json
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, world!\nIt's flask-contab app test!"
+
+
+@app.route('/callbacks', methods=['GET', 'POST'])
+def spotify():
+    return json.dumps(request.args)
 
 
 api.add_resource(Time, "/time")
